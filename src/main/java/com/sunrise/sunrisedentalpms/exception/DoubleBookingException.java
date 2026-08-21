8ae3782760583c2be4b92cb787074422eb6c3fb4
PavithRajapaksha <sartharajapaksha2000@gmail.com>
2026-08-21 23:52:21 +0530

    added an exception to be thrawn, when a booking is overlapping the other's date and time.

---
+++ src/main/java/com/sunrise/sunrisedentalpms/exception/DoubleBookingException.java
@@ -0,0 +1,8 @@
+package com.sunrise.sunrisedentalpms.exception;
+
+public class DoubleBookingException extends Exception {
+
+    public DoubleBookingException(String message) {
+        super(message);
+    }
+}
